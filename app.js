7e74c59cfa3a3301aa3bceb59ed824def31ea623
unknown <user.name>
2022-07-12 11:10:38 +0200

    cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const helmet = require('helmet');
 const routes = require('./routes/index');
 const error = require('./middlewares/error');
@@ -10,13 +11,29 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const rateLimiter = require('./utils/rateLimiter');
 
 const app = express();
+require('dotenv').config();
 const { PORT = 3000 } = process.env;
 mongoose.connect(NODE_ENV === 'production' ? MONGO_URL : 'mongodb://localhost:27017/news-explorer', {
   useNewUrlParser: true,
 });
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(requestLogger);
+app.use((req, res, next) => {
+  res.header(
+    'Access-Control-Allow-Origin',
+    'https://pep.students.nomoreparties.sbs',
+  );
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, authorization',
+  );
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  next();
+});
+app.use(cors());
+app.options('*', cors());
 app.use(rateLimiter);
 
 app.use(routes);
